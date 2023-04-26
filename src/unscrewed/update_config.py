d99b75f3b1281d5646fa6ec46d5728261a138b57
Matthew Brett <matthew.brett@gmail.com>
2023-04-26 13:24:50 +0100

    Building up update_config machinery

--- src/unscrewed/update_config.py
+++ src/unscrewed/update_config.py
@@ -2,31 +2,89 @@
 """
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from urllib.parse import urlparse
+from subprocess import check_call
+from tempfile import TemporaryDirectory
+from pathlib import Path
 
 import yaml
 
 from .fetcher import Fetcher
 
 
-def update_config(config, repo_url,
-                  repo_dir=None,
-                  external_dir=None,
+def update_config(config,
+                  repos_path,
                   default_hasher='md5'):
     config = Fetcher(config).config
-    repo_dir = get_check_repo(config, repo_url, repo_dir)
-    # Calculate hashes for each file not ignored, set
-    config['files'], config['urls'] = calc_hashes(config, repo_dir,
-                                                  external_dir,
-                                                  default_hasher)
-    # Consider checking external files.
+    file_defs = get_file_defs(config)
+    repos_path.mkdir(parents=True, exist_ok=True)
+    return config   # For now.
+    check_add_repos(file_defs, repos_path)
+    fill_check_hashes(file_defs, default_hasher)
+    fill_config(config, file_defs)
     return config
 
+def get_file_defs(config):
+    return config['files']
 
-def get_check_repo(config, repo_url, repo_dir):
-    # If repo_dir is None, clone data repository to tempory
-    # Look for cloned version of repository.
-    # Check repo is clean, pushed.
-    return repo_dir
+
+def check_add_repos(file_defs, repos_path):
+    for fname, info in file_defs.items():
+        # Modify dictionary in-place.
+        info['repo_path'], info['rel_path'] = check_add_repo(
+            info['url'],
+            info['version'],
+            repos_path)
+
+
+class RepoError(ValueError):
+    """ Signal error analyzing repository URL """
+
+
+def check_add_repo(url, version, repos_path):
+    cloneable, url_version, relpath = url2cloneable(url)
+    if cloneable is None:
+        return None, None
+    if version:
+        if url_version != '{version}':
+            raise RepoError(
+                f'Requested version is {version} but URL has {url_version}')
+        url_version = version
+    elif url_version == '{version}':
+        raise RepoError(
+            f'No requested version, but URL {url} contains'
+            '{version}')
+    return check_clone(cloneable, url_version, repos_path), relpath
+
+
+def check_clone(cloneable, version, repos_path):
+    out_path = repos_path / cloneable2out_sdir(cloneable)
+    if out_path.is_dir():
+        # Check version
+        pass
+    else:
+        check_call(['git', 'clone', cloneable, str(out_path)])
+    return out_path
+
+
+def cloneable2out_sdir(cloneable):
+    out_sdir = [p for p in cloneable.split('/') if p][-1]
+    if out_sdir.endswith('.git'):
+        out_sdir = out_sdir[:-4]
+    return out_sdir
+
+
+def url2cloneable(url):
+    parts = urlparse(url)
+    if parts.scheme != 'https':
+        raise RepoError(f'Need https: scheme, but URL is {url}')
+    if parts.netloc == 'raw.githubusercontent.com':
+        # Github raw file.
+        fparts = parts.path.split('/')
+        return ('https://github.com' + '/'.join(fparts[:3]),
+                fparts[3],
+                '/'.join(fparts[4:]))
+    return None, None, None
 
 
 def calc_hashes(config, repo_dir, external_dir=None, default_hasher='md5'):
@@ -43,7 +101,7 @@ def get_parser():
                             formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('config_fname',
                         help='Configuration filename')
-    parser.add_argument('-d', '--repo-dir',
+    parser.add_argument('-d', '--repos-dir',
                         help='Directory in which to clone repositories')
     return parser
 
@@ -51,5 +109,7 @@ def get_parser():
 def cli():
     parser = get_parser()
     args = parser.parse_args()
-    config = update_config(args.config_fname, repo_dir=args.repo_dir)
+    if args.repos_dir is None:
+        args.repos_dir = Path(TemporaryDirectory().name)
+    config = update_config(args.config_fname, repos_dir=args.repos_dir)
     write_config(config, args.config_fname)
--- tests/test_update_config.py
+++ tests/test_update_config.py
@@ -3,14 +3,59 @@
 
 from pathlib import Path
 
-import unscrewed.update_config as usu
+from unscrewed.update_config import (
+    update_config,
+    RepoError,
+    url2cloneable,
+    check_add_repo,
+    cloneable2out_sdir,
+)
 import unscrewed.fetcher as usf
+
+import pytest
 
 
 CONFIG_PATH = Path(__file__).parent / 'testreg_registry.yaml'
 
 
-def test_config():
+def test_config(tmp_path):
     config = usf.Fetcher(CONFIG_PATH).config
-    new_config = usu.update_config(config, None)
+    new_config = update_config(config, tmp_path)
     assert config == new_config
+
+
+def test_url2cloneable():
+    base_url = ("https://raw.githubusercontent.com/"
+                "matthew-brett/unscrewed/{version}/tests/testreg")
+    assert url2cloneable(base_url) == (
+        'https://github.com/matthew-brett/unscrewed',
+        '{version}',
+        'tests/testreg')
+    with pytest.raises(RepoError):
+        url2cloneable('git' + base_url[5:])
+
+
+def test_check_add_repo(tmp_path):
+    base_url = ("https://raw.githubusercontent.com/"
+                "matthew-brett/unscrewed/{version}/tests/testreg")
+    with pytest.raises(RepoError):
+        check_add_repo(base_url.replace('{version}', '0.3a1'),
+                       '0.1a2',
+                       tmp_path)
+    with pytest.raises(RepoError):
+        check_add_repo(base_url,
+                       None,
+                       tmp_path)
+    out_path, rel_path = check_add_repo(base_url,
+                                        '0.1a2',
+                                        tmp_path)
+    assert out_path.is_dir()
+    assert rel_path == 'tests/testreg'
+
+
+def test_cloneable2out_dir():
+    assert cloneable2out_sdir('https://foo.bar/baz') == 'baz'
+    assert cloneable2out_sdir('https://foo.bar/baz/') == 'baz'
+    assert cloneable2out_sdir('https://foo.bar/baz.git') == 'baz'
+    assert cloneable2out_sdir('https://foo.bar/baz/boo.git') == 'boo'
+    assert cloneable2out_sdir('https://foo.bar/baz/boo.git/') == 'boo'
